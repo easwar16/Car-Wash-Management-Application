bfbe557f946fc9bdf9de40269987ca9c4a791a80
Easwar H <easwarharikaran1610@gmail.com>
2023-01-17 21:43:37 +0530

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,90 @@
+        <!-- Top Bar Start -->
+        <div class="top-bar">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-4 col-md-12">
+                        <div class="logo">
+                            <a href="index.php">
+                                <h1>CAR <span>Wash</span></h1>
+                            </a>
+                        </div>
+                    </div>
+
+<?php 
+$sql = "SELECT * from tblpages where type='contact'";
+$query = $dbh -> prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+foreach($results as $result)
+{       
+?>
+                    <div class="col-lg-8 col-md-7 d-none d-lg-block">
+                        <div class="row">
+                            <div class="col-4">
+                                <div class="top-bar-item">
+                                    <div class="top-bar-icon">
+                                        <em class="fa fa-clock"></em>
+                                    </div>
+                                    <div class="top-bar-text">
+                                        <h3>Opening Hour</h3>
+                                        <p>Mon - Fri, 8:00 AM - 9:00 PM</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="top-bar-item">
+                                    <div class="top-bar-icon">
+                                        <em class="fa fa-phone-alt"></em>
+                                    </div>
+                                    <div class="top-bar-text">
+                                        <h3>Call Us</h3>
+                                        <p>+91 9789027597</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="top-bar-item">
+                                    <div class="top-bar-icon">
+                                        <em class="far fa-envelope"></em>
+                                    </div>
+                                    <div class="top-bar-text">
+                                        <h3>Email Us</h3>
+                                        <p>Car Wash</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+                </div>
+            </div>
+        </div>
+        
+        
+        <div class="nav-bar">
+            <div class="container">
+                <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+                    <a href="#" class="navbar-brand">MENU</a>
+                    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                        <div class="navbar-nav mr-auto">
+                            <a href="index.php" class="nav-item nav-link active">Home</a>
+                            <a href="about.php" class="nav-item nav-link">About</a>
+                            <a href="washing-plans.php" class="nav-item nav-link">Washing Plans</a>
+                            <a href="location.php" class="nav-item nav-link">Washing Points</a>
+                    
+                            <a href="admin" class="nav-item nav-link">admin</a>
+                        </div>
+                        <div class="ml-auto">
+                            <a class="btn btn-custom" href="Login.php">LOGIN</a>
+                        </div>
+                        <div class="ml-auto">
+                            <a class="btn btn-custom" href="Signup.php">SIGN UP</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
